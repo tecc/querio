5a832185c65be4f1566a30fc02b1e15af2c388a3
tecc <tecc@tecc.me>
2023-07-31 01:43:43 +0200

    test(optimise): Add tests for OR and AND reductions

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "@jest/globals";
-import type { Condition } from "./index";
+import type { BuiltinCondition, Condition } from "./index";
 import { optimise, parse, values } from "./index";
 
 const spec = {
@@ -15,11 +15,11 @@ const spec = {
 };
 
 type Spec = typeof spec;
+type Cond = Condition<Spec>;
 
 describe("parsing", () => {
     it("simple top-level query", () => {
-        const result = parse("a:value b c d:24 e", spec);
-        expect(result).toEqual({
+        expect(parse("a:value b c d:24 e", spec)).toEqual({
             type: "and",
             operands: [
                 {
@@ -40,12 +40,11 @@ describe("parsing", () => {
                     type: "e"
                 }
             ]
-        } satisfies Condition<Spec>);
+        } satisfies Cond);
     });
 
     it("top-level OR", () => {
-        const result = parse("a:value b | c d:24 e", spec);
-        expect(result).toEqual({
+        expect(parse("a:value b | c d:24 e", spec)).toEqual({
             type: "or",
             operands: [
                 {
@@ -76,12 +75,12 @@ describe("parsing", () => {
                     ]
                 }
             ]
-        } satisfies Condition<Spec>);
+        } satisfies Cond);
     });
 });
 describe("optimising", () => {
     it("double-negation", () => {
-        const inner = {
+        const inner: Cond = {
             type: "a",
             value: "something"
         };
@@ -96,7 +95,7 @@ describe("optimising", () => {
         ).toEqual(inner);
 
         expect(
-            optimise({
+            optimise<Spec>({
                 type: "not",
                 operand: {
                     type: "not",
@@ -110,6 +109,67 @@ describe("optimising", () => {
             type: "not",
             operand: inner
         });
+    });
+    describe("ORs/ANDs operators with differently-sized elements", () => {
+        for (const op of ["and", "or"] as const) {
+            it(`${op}: with no elements`, () => {
+                const empty: Cond = {
+                    type: op,
+                    operands: []
+                };
+                expect(optimise<Spec>(empty)).toBeNull();
+                expect(
+                    optimise<Spec>({
+                        type: op,
+                        operands: [empty]
+                    })
+                ).toBeNull();
+                expect(
+                    optimise<Spec>({
+                        type: op,
+                        operands: [empty, empty]
+                    })
+                ).toBeNull();
+            });
+            it(`${op}: with one element`, () => {
+                const single: Cond = {
+                    type: "a",
+                    value: "something"
+                };
+                expect(
+                    optimise<Spec>({
+                        type: op,
+                        operands: [single]
+                    })
+                ).toEqual(single);
+                expect(
+                    optimise<Spec>({
+                        type: op,
+                        operands: [
+                            {
+                                type: op,
+                                operands: [single]
+                            }
+                        ]
+                    })
+                ).toEqual(single);
+            });
+            it(`${op}: with more than one element`, () => {
+                const original: Cond = {
+                    type: op,
+                    operands: [
+                        {
+                            type: "a",
+                            value: "something"
+                        },
+                        {
+                            type: "b"
+                        }
+                    ]
+                };
+                expect(optimise<Spec>(original)).toEqual(original);
+            });
+        }
     });
 });
 
